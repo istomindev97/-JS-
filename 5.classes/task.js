13179127dc3b66326532624c29045996fb625fbb
Ilya Istomin <istominilya97@gmail.com>
2024-03-20 14:52:29 +0300

    Пятое задание частично готово

---
+++ 5.classes/task.js
@@ -0,0 +1,117 @@
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this.state = 100;
+        this.type = null;
+    }
+
+    fix() {
+        let newState = this.state * 1.5;
+        if ( newState > 100) {
+            newState = 100;
+        }   
+        this.state = newState;
+    }
+
+
+    set state (newState) {
+      
+    if (newState < 0) {
+        this._state = 0;
+      } else if (newState > 100) {
+        this._state = 100;
+      } else {
+        this._state = newState;
+      }
+    }
+
+    get state () {
+        return this._state;
+    }
+}
+
+class Magazine extends PrintEditionItem {
+    constructor (name, releaseDate, pagesCount) {
+        super (name, releaseDate, pagesCount);
+        this.type = 'magazine';
+    }
+}
+
+class Book extends PrintEditionItem {
+    constructor (author, name, releaseDate, pagesCount) {
+        super (name, releaseDate, pagesCount);
+        this.author = author;
+        this.type = 'book';
+        this.state = 100;
+    }
+}
+
+class NovelBook  extends Book {
+    constructor (author, name, releaseDate, pagesCount) {
+        super (name, releaseDate, pagesCount);
+        this.author = author;
+        this.type = 'novel';
+    }
+}
+
+class FantasticBook  extends Book {
+    constructor (author, name, releaseDate, pagesCount) {
+        super (name, releaseDate, pagesCount);
+        this.author = author;
+        this.type = 'fantastic';
+    }
+}
+
+class DetectiveBook  extends Book {
+    constructor (author, name, releaseDate, pagesCount) {
+        super (name, releaseDate, pagesCount);
+        this.author = author;
+        this.type = 'detective';
+    }
+}
+
+class Library {
+    constructor (name, books) {
+        this.name = name;
+        this.books = books;
+    }
+
+    addBook(book) {
+        if (book.state > 30) {
+            this.books.push(book);
+        }
+    }
+
+    findBookBy(type, value) {
+        //Непонятно что тут должно работать
+    }
+
+    giveBookByName(bookName) {
+        if (this.books.includes(bookName)) {
+            this.books.splice(indexOf(bookName), 1);
+            return bookName;
+        }
+
+        return null;
+    }
+}
+
+
+const picknick = new FantasticBook(
+    "Аркадий и Борис Стругацкие",
+    "Пикник на обочине",
+    1972,
+    168
+  );
+
+
+  const library = new Library("Библиотека имени Ленина");
+
+ let sherlock =  new DetectiveBook(
+    "Артур Конан Дойл",
+    "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+    2019,
+    1008
+  )
